361ca2b591909c70ea20c061e40c3ff1eefec2fc
Abdelrahman Kamal <abdelrahman212k@gmail.com>
2023-11-18 02:08:18 +0200

    Adding library files

---
+++ LIB/DEFINES.h
@@ -0,0 +1,30 @@
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define NULL    ((void*) 0)
+
+#ifndef FALSE
+#define	FALSE   0U
+#endif
+
+#ifndef	TRUE
+#define	TRUE   1U
+#endif
+
+#ifndef ENABLED
+#define ENABLED     1U
+#endif
+
+#ifndef DISABLED
+#define DISABLED    2U
+#endif
+
+#ifndef	BUSY
+#define BUSY		1U
+#endif
+
+#ifndef	IDLE
+#define IDLE		0U
+#endif
+
+#endif /* DEFINES_H_ */
